65c4d52895ef1ad28fb5b9ff95132c98992905d9
victoria <vikabirukova22@gmail.com>
2024-01-28 11:10:31 +0200

    Add controllers for pet

--- server/controllers/petsController.js
+++ server/controllers/petsController.js
@@ -12,7 +12,7 @@ module.exports.createPet = async (req, res, next) => {
       return next(createHttpError(400, 'Something went wrong...'));
     }
 
-    const preparedPet = OMIT(createdPet.get())
+    const preparedPet = OMIT(createdPet.get());
 
     res.status(201).send(preparedPet);
   } catch (err) {
@@ -39,8 +39,66 @@ module.exports.getPets = async (req, res, next) => {
   }
 };
 
-module.exports.getPetById = async (req, res, next) => {};
+module.exports.getPetById = async (req, res, next) => {
+  const { id } = req.params;
 
-module.exports.updatePetById = async (req, res, next) => {};
+  try {
+    const foundPet = await Pet.findByPk(id, {
+      raw: true,
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
 
-module.exports.deletePetById = async (req, res, next) => {};
+    if (!foundPet) {
+      return next(createHttpError(404, 'Pet not found ):'));
+    }
+
+    res.status(200).send(foundPet);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updatePetById = async (req, res, next) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+  console.log(id);
+
+  try {
+    const [updatedPetCount, [updatedPet]] = await Pet.update(body, {
+      where: { id },
+      returning: true,
+    });
+
+    if (!updatedPetCount) {
+      return next(createHttpError(404, 'Pet not found );'));
+    }
+
+    const preparedPet = OMIT(updatedPet.get());
+
+    res.status(200).send(preparedPet);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deletePetById = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const deletedPet = await Pet.destroy({
+      where: { id },
+    });
+
+    if (!deletedPet) {
+      return next(createHttpError(404, 'Pet not found'));
+    }
+
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+};
